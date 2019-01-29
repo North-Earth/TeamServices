05464b818db84fdc878dd2ca294d81889d0787b7
Revolt27 <kuckushckin.alex@yandex.ru>
2019-01-30 00:21:39 +0300

    Актуализированы скрипты наката и отката

--- mssql/Scripts/deploy.sql
+++ mssql/Scripts/deploy.sql
@@ -9,99 +9,20 @@ GO
 CREATE SCHEMA TeamServices
 GO
 
-/*** Таблица с информацией о статических файлах ***/
-
-DROP TABLE IF EXISTS TeamServices.Files
-GO
-
-CREATE TABLE TeamServices.Files
-(
-     Id          INTEGER IDENTITY(1,1)  NOT NULL
-    ,Name        NVARCHAR(256)          NOT NULL
-    ,Description NVARCHAR(1024)             NULL
-    ,FileName    NVARCHAR(256)          NOT NULL
-    ,CONSTRAINT PkFiles PRIMARY KEY CLUSTERED 
-    (
-	    Id ASC
-    )
-)
-GO
-
-INSERT INTO TeamServices.Files
-(
-     Name
-    ,Description
-    ,FileName    
-)
-VALUES
-(
-     'Отсутствие на рабочем месте'
-    ,'Заявление об отсутствии сотрудника на рабочем месте.'
-    ,'Шаблон отсутствие на рабочем месте.docx'
-),
-(
-     'Неоплачиваемый отпуск'
-    ,'Заявление на неоплачиваемый отпуск.'
-    ,'Шаблон ежегодный оплачиваемый отпуск.docx'
-),
-(
-     'Оплачиваемый отпуск'
-    ,'Заявление на ежегодный отпуск с сохранением заработной платы.'
-    ,'Шаблон неоплачиваемый отпуск.docx'
-)
-GO
-
-/***  Таблица с информацией о пользователях ***/
-
-DROP TABLE IF EXISTS TeamServices.Staff
-
-CREATE TABLE TeamServices.Staff
-(
-     Id              INTEGER IDENTITY(1,1) NOT NULL
-    ,MachineName     NVARCHAR(256)         NOT NULL
-    ,Name            NVARCHAR(256)         NOT NULL
-    ,Birthday        DATE                  NOT NULL
-    ,IpAddress       NVARCHAR(16)          NOT NULL
-    ,CONSTRAINT PkUsers PRIMARY KEY CLUSTERED 
-    (
-	    Id ASC
-    )
-)
-
-INSERT INTO TeamServices.Staff
-(
-     MachineName
-    ,Name
-    ,Birthday
-    ,IpAddress 
-)
-VALUES
-(
-     'Earth'
-    ,'Кукушкин Алексей Александрович'
-    ,'1998-02-27'
-    ,'192.168.1.179'
-),
-(
-     'Venus'
-    ,'Замчалова Кристина Витальевна'
-    ,'2000-03-10'
-    ,'192.168.1.93'
-)
-
 /*** Командный справочник ***/
 
 DROP TABLE IF EXISTS TeamServices.Dictionary
+GO
 
 CREATE TABLE TeamServices.Dictionary
 (
      Id            INTEGER IDENTITY(1,1) NOT NULL
-    ,Name          NVARCHAR(256)         NOT NULL
-    ,Description   NVARCHAR(1024)            NULL
-    ,SqlExpression NVARCHAR(2048)        NOT NULL
+    ,Name          NVARCHAR(64)          NOT NULL
+    ,Description   NVARCHAR(256)             NULL
+    ,SqlExpression NVARCHAR(1024)        NOT NULL
     ,CONSTRAINT PkDictionary PRIMARY KEY CLUSTERED 
     (
-	    Id ASC
+        Id ASC
     )
 )
 GO
@@ -114,17 +35,45 @@ INSERT INTO TeamServices.Dictionary
 )
 VALUES
 (
-     'Командный справочник'
-    ,'Командный справочник с полезными запросами'
-    ,'SELECT * FROM Development.TeamServices.Dictionary'
+     '[TeamToolkit] Командный справочник'
+    ,'Справочник с описанием источников командных проектов.'
+    ,'SELECT * FROM Production.TeamServices.Dictionary'
+)
+,(
+     '[TeamToolkit] Командный файловый репозиторий'
+    ,'Справочник с описаинем командных файлов'
+    ,'SELECT * FROM Production.TeamServices.Files'
+)
+,(
+     '[TeamToolkit] Ссылки'
+    ,'Источник с ссылками для инструментария'
+    ,'SELECT * FROM Production.TeamServices.Links'
+)
+,(
+     '[TeamToolkit] Командные отчёты'
+    ,'Справочник очётов переработок и недоработок членов команды'
+    ,'SELECT * FROM Production.TeamServices.WorkReport'
+)
+,(
+     '[TeamToolkit] Командные проекты'
+    ,'Справочник с проектами команды'
+    ,'SELECT * FROM Production.TeamServices.Projects'
+)
+,(
+     '[TeamToolkit] Цитатник'
+    ,'Забавные цитаты членов команды'
+    ,'SELECT * FROM Production.TeamServices.Quotes'
+)
+,(
+     '[TeamToolkit] Информация о персонале'
+    ,'Содержит в себе подробную информацию о персонале'
+    ,'SELECT * FROM Production.TeamServices.Staff'
 )
 
-/*** Витрина справочника ***/
-
-DROP VIEW IF EXISTS TeamServices.vDictionary
+DROP VIEW IF EXISTS TeamServices.DictionaryView
 GO
 
-CREATE VIEW TeamServices.vDictionary
+CREATE VIEW TeamServices.DictionaryView
 AS
 SELECT
      dct.Id
@@ -134,34 +83,289 @@ SELECT
 FROM TeamServices.Dictionary AS dct
 GO
 
-/*** Витрина файлового репозитория ***/
+/*** Справочник файлового репозитория ***/
 
-DROP VIEW IF EXISTS TeamServices.vFiles
+DROP TABLE IF EXISTS TeamServices.Files
 GO
 
-CREATE VIEW TeamServices.vFiles
+CREATE TABLE TeamServices.Files
+(
+     Id          INTEGER IDENTITY(1,1)  NOT NULL
+    ,Name        NVARCHAR(256)          NOT NULL
+    ,Description NVARCHAR(256)              NULL
+    ,FileName    NVARCHAR(256)          NOT NULL
+    ,CONSTRAINT PkFiles PRIMARY KEY CLUSTERED 
+    (
+        Id ASC
+    )
+)
+GO
+
+DROP VIEW IF EXISTS TeamServices.FilesView
+GO
+
+CREATE VIEW TeamServices.FilesView
 AS
-SELECT 
+SELECT
      fls.Id
     ,fls.Name
     ,fls.Description
-    ,fls.FileName 
+    ,fls.FileName
 FROM TeamServices.Files AS fls
 GO
 
-/*** Витрина информации о пользователях ***/
-
-DROP VIEW IF EXISTS TeamServices.vUsers
+INSERT INTO TeamServices.Files
+(
+     Name
+    ,Description
+    ,FileName
+)
+VALUES
+(
+     'Отсутствие на рабочем месте'
+    ,'Заявление об отсутствии сотрудника на рабочем месте.'
+    ,'Шаблон отсутствие на рабочем месте.docx'
+)
+,(
+     'Неоплачиваемый отпуск'
+    ,'Заявление на неоплачиваемый отпуск.'
+    ,'Шаблон ежегодный оплачиваемый отпуск.docx'
+)
+,(
+     'Оплачиваемый отпуск'
+    ,'Заявление на ежегодный отпуск с сохранением заработной платы.'
+    ,'Шаблон неоплачиваемый отпуск.docx'
+)
 GO
 
-CREATE VIEW TeamServices.vUsers
+/*** Справочник с ссылками проектов ***/
+
+DROP TABLE IF EXISTS TeamServices.Links
+GO
+
+CREATE TABLE TeamServices.Links
+(
+     Id       INTEGER       NOT NULL /* ID из TeamServices.Projects */
+    ,TypeId   TINYINT       NOT NULL /* Тип ссылки 0 - общая, 1 - Git, 2 - Confluence TODO: Сделать справочник с типами*/
+    ,Name     NVARCHAR(32)  NOT NULL
+    ,Url      NVARCHAR(256) NOT NULL
+    ,IcoClass VARCHAR(16)       NULL /* Класс иконки из пакета стелей FontAwesome */
+)
+GO
+
+DROP VIEW IF EXISTS TeamServices.LinksView
+GO
+
+CREATE VIEW TeamServices.LinksView
 AS
 SELECT
-     usr.Id             
-    ,usr.MachineName    
-    ,usr.Name           
-    ,usr.Birthday       
-    ,usr.IpAddress      
-    --,usr.HardwareAddress
-FROM TeamServices.Users AS usr
+     lnk.Id
+    ,TypeId
+    ,lnk.Name
+    ,lnk.Url
+    ,lnk.IcoClass
+FROM TeamServices.Links AS lnk
+GO
+
+INSERT INTO TeamServices.Links
+(
+     lnk.Id
+    ,TypeId
+    ,lnk.Name
+    ,lnk.Url
+    ,lnk.IcoClass
+)
+VALUES
+(
+     1
+    ,1
+    ,'GitHub'
+    ,'#'
+    ,NULL
+),
+(
+     1
+    ,2
+    ,'Confluence'
+    ,'#'
+    ,NULL
+)
+,(
+     0
+    ,0
+    ,'Metanit'
+    ,'https://metanit.com/'
+    ,'fa fa-tasks'
+)
+GO
+
+/*** Справочник отработок и переработок команды ***/
+
+DROP TABLE IF EXISTS TeamServices.WorkReport
+GO
+
+CREATE TABLE TeamServices.WorkReport
+(
+     Name           NVARCHAR(256) NOT NULL
+    ,Description    NVARCHAR(256) NOT NULL
+    ,UserName       VARCHAR (128) NOT NULL
+    ,TimeHour       SMALLINT      NOT NULL
+    ,LoadDtm        DATETIME2(3)  NOT NULL
+)
+
+DROP VIEW IF EXISTS TeamServices.WorkReportView
+GO
+
+CREATE VIEW TeamServices.WorkReportView
+AS
+SELECT
+     rpt.Name
+    ,rpt.Description
+    ,rpt.UserName
+    ,rpt.TimeHour
+    ,rpt.LoadDtm
+FROM TeamServices.WorkReport AS rpt
+GO
+
+/*** Справочник командных проектов ***/
+
+DROP TABLE IF EXISTS TeamServices.Projects
+GO
+
+CREATE TABLE TeamServices.Projects
+(
+     Id          INTEGER IDENTITY(1,1)  NOT NULL
+    ,Name        NVARCHAR(32)           NOT NULL
+    ,Description NVARCHAR(256)              NULL
+    ,LogoPath    NVARCHAR(256)              NULL
+    ,CONSTRAINT PkProjects PRIMARY KEY CLUSTERED
+    (
+        Id ASC
+    )
+)
+GO
+
+DROP VIEW IF EXISTS TeamServices.ProjectsView
+GO
+
+CREATE VIEW TeamServices.ProjectsView
+AS
+SELECT
+     prj.Id
+    ,prj.Name
+    ,prj.Description
+    ,prj.LogoPath
+FROM TeamServices.Projects AS prj
+GO
+
+INSERT INTO TeamServices.Projects
+(
+     prj.Name
+    ,prj.Description
+    ,prj.LogoPath
+)
+VALUES
+(
+     'Team Toolkit'
+    ,'Командный инструментарий'
+    ,'prjLogo1.jpg'
+)
+,(
+     'Test Project'
+    ,'Тестовый проект'
+    ,'prjLogo3.jpg'
+)
+GO
+
+/*** Забавные цитаты членов команды ***/
+
+DROP TABLE IF EXISTS TeamServices.Quotes
+GO
+
+CREATE TABLE TeamServices.Quotes
+(
+     id      INTEGER IDENTITY(1,1) NOT NULL
+    ,text    NVARCHAR(256)         NOT NULL
+    ,author  NVARCHAR(64)          NOT NULL
+    ,CONSTRAINT PkQuotes PRIMARY KEY CLUSTERED
+    (
+        id ASC
+    )
+)
+GO
+
+DROP VIEW IF EXISTS TeamServices.QuotesView
+GO
+
+CREATE VIEW TeamServices.QuotesView
+AS
+SELECT
+     qts.id
+    ,qts.text
+    ,qts.author
+FROM TeamServices.Quotes AS qts
+GO
+
+INSERT INTO TeamServices.QuotesView
+(
+     text
+    ,author
+)
+VALUES
+(
+     'TRUNACATE коронный, DROP TABLE похоронный'
+    ,'Кукушкин Алексей Александрович'
+)
+GO
+
+/*** Информация о персонале ***/
+
+DROP TABLE IF EXISTS TeamServices.Staff
+GO
+
+CREATE TABLE TeamServices.Staff
+(
+     Id              INTEGER IDENTITY(1,1) NOT NULL
+    ,UserName        VARCHAR(64)           NOT NULL
+    ,MachineName     VARCHAR(64)           NOT NULL
+    ,Name            NVARCHAR(64)          NOT NULL
+    ,Birthday        DATE                  NOT NULL
+    ,IpAddress       VARCHAR(16)           NOT NULL
+    ,CONSTRAINT PkUsers PRIMARY KEY CLUSTERED 
+    (
+        Id ASC
+    )
+)
+
+DROP VIEW IF EXISTS TeamServices.StaffView
+GO
+
+CREATE VIEW TeamServices.StaffView
+AS
+SELECT
+     stf.Id
+    ,stf.UserName
+    ,stf.MachineName
+    ,stf.Name
+    ,stf.Birthday
+    ,stf.IpAddress
+FROM TeamServices.Staff AS stf
+GO
+
+INSERT INTO TeamServices.Staff
+(
+     UserName
+    ,MachineName
+    ,Name
+    ,Birthday
+    ,IpAddress
+)
+VALUES
+(
+     'kukushkin_aa'
+    ,'Venus'
+    ,'Кукушкин Алексей Александрович'
+    ,'1998-02-27'
+    ,'192.168.1.93'
+)
 GO
--- mssql/Scripts/rollback.sql
+++ mssql/Scripts/rollback.sql
@@ -1,30 +1,42 @@
 USE <<SERVERNAME>>
 GO
 
-/***  ***/
-
-DROP TABLE IF EXISTS TeamServices.<<NAME>>
+DROP TABLE IF EXISTS TeamServices.Dictionary
 GO
-
-/***  ***/
-
-DROP VIEW IF EXISTS TeamServices.<<NAME>>
+DROP VIEW IF EXISTS TeamServices.DictionaryView
 GO
-
-/**************************************************/
-
-/*** Таблица с информацией о статических файлах ***/
 
 DROP TABLE IF EXISTS TeamServices.Files
 GO
-
-/*** Витрина с информацией о статических файлах ***/
-
 DROP VIEW IF EXISTS TeamServices.FilesView
 GO
 
 
-/*** Схема для объектов командных сервисов ***/
+DROP TABLE IF EXISTS TeamServices.Links
+GO
+DROP VIEW IF EXISTS TeamServices.LinksView
+GO
+
+DROP TABLE IF EXISTS TeamServices.WorkReport
+GO
+DROP VIEW IF EXISTS TeamServices.WorkReportView
+GO
+
+DROP TABLE IF EXISTS TeamServices.Projects
+GO
+DROP VIEW IF EXISTS TeamServices.ProjectsView
+GO
+
+
+DROP TABLE IF EXISTS TeamServices.Quotes
+GO
+DROP VIEW IF EXISTS TeamServices.QuotesView
+GO
+
+DROP TABLE IF EXISTS TeamServices.Staff
+GO
+DROP VIEW IF EXISTS TeamServices.StaffView
+GO
 
 DROP SCHEMA IF EXISTS TeamServices
 GO
